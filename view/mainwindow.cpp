64e08c2cb802b2afbe318f55a651f4a45021ef1f
Ydream <995862798@qq.com>
2020-07-13 11:03:50 +0800

    增加基础样式

--- view/mainwindow.cpp
+++ view/mainwindow.cpp
@@ -21,17 +21,47 @@ void MainWindow::InitPlayerWidget()
 {
     ui->playerDataWidget->setRowCount(6);
     ui->playerDataWidget->setColumnCount(2);
+    QIcon playerImg;
+    playerImg.addFile(":/assets/Images/415_.png");
+    ui->playerDataWidget->setItem(0,0,new QTableWidgetItem(playerImg,""));
     ui->playerDataWidget->setItem(1,0,new QTableWidgetItem("生命"));
+    ui->playerDataWidget->setItem(1,1,new QTableWidgetItem("1000"));
     ui->playerDataWidget->setItem(2,0,new QTableWidgetItem("攻击"));
+    ui->playerDataWidget->setItem(2,1,new QTableWidgetItem("0"));
     ui->playerDataWidget->setItem(3,0,new QTableWidgetItem("防御"));
+    ui->playerDataWidget->setItem(3,1,new QTableWidgetItem("0"));
     ui->playerDataWidget->setItem(4,0,new QTableWidgetItem("金币"));
+    ui->playerDataWidget->setItem(4,1,new QTableWidgetItem("0"));
     ui->playerDataWidget->setItem(5,0,new QTableWidgetItem("经验"));
+    ui->playerDataWidget->setItem(5,1,new QTableWidgetItem("0"));
+    ui->playerDataWidget->setIconSize(QSize(60,60));
     ui->playerDataWidget->horizontalHeader()->setHidden(true);
     ui->playerDataWidget->verticalHeader()->setHidden(true);
     ui->playerDataWidget->setShowGrid(false);
     ui->playerDataWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
     ui->playerDataWidget->setSelectionMode(QAbstractItemView::NoSelection);
     ui->playerDataWidget->setFocusPolicy(Qt::NoFocus);
+    ui->playerDataWidget->setColumnWidth(0,100);
+    ui->playerDataWidget->setColumnWidth(1,100);
+
+    ui->playerDataWidget->setRowHeight(0,80);
+    ui->playerDataWidget->setRowHeight(1,40);
+    ui->playerDataWidget->setRowHeight(2,40);
+    ui->playerDataWidget->setRowHeight(3,40);
+    ui->playerDataWidget->setRowHeight(4,40);
+    ui->playerDataWidget->setRowHeight(5,40);
+    for(int i=1;i<6;i++)
+        for(int j=0;j<2;j++)
+        {
+            if(j==1)
+            {
+                QFont numFont;
+                numFont.setItalic(true);
+                ui->playerDataWidget->item(i,j)->setFont(numFont);
+            }
+            ui->playerDataWidget->item(i,j)->setTextAlignment(Qt::AlignCenter);
+        }
+
 }
 void MainWindow::InitKeysWidget()
 {
@@ -60,4 +90,19 @@ void MainWindow::InitOptionsWidget()
     ui->optionsDataWidget->addItem("ctrl A 读取    ctrl R 重新开始");
     ui->optionsDataWidget->setSelectionMode(QAbstractItemView::NoSelection);
     ui->optionsDataWidget->setFocusPolicy(Qt::NoFocus);
+}
+
+std::shared_ptr<QVector<int>> MainWindow::getPtrToPlayerData()
+{
+    return ptrToPlayerData;
+}
+
+std::shared_ptr<QVector<int>> MainWindow::getPtrToKeysData()
+{
+    return ptrToKeysData;
+}
+
+std::shared_ptr<int> MainWindow::getPtrToLevelData()
+{
+    return ptrToLevelData;
 }
--- view/mainwindow.h
+++ view/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include "magicmap.h"
+#include "memory"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,11 +17,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    std::shared_ptr<QVector<int>> getPtrToPlayerData();
+    std::shared_ptr<QVector<int>> getPtrToKeysData();
+    std::shared_ptr<int> getPtrToLevelData();
+
 private:
     Ui::MainWindow *ui;
     void InitPlayerWidget();
     void InitKeysWidget();
     void InitLevelWidget();
     void InitOptionsWidget();
+
+    std::shared_ptr<QVector<int>> ptrToPlayerData;
+    std::shared_ptr<QVector<int>> ptrToKeysData;
+    std::shared_ptr<int> ptrToLevelData;
 };
 #endif // MAINWINDOW_H
